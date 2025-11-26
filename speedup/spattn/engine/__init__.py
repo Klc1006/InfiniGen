1e00859505cd65911e9682f3be1578c4290190c5
clk <chenlk8@mail2.sysu.edu.cn>
2025-11-27 00:26:34 +0800

    delete original engine

--- speedup/spattn/engine/__init__.py
+++
@@ -1,4 +0,0 @@
-from .engines import EngineRegular, EngineRegularDummy  # noqa: F401
-from .engines_static import EngineStatic  # noqa: F401
-from .EngineFast import InferenceEngine, InferenceEngineTG, GraphInferenceEngine, GraphInferenceEngineTG, InferenceEnginePadded  # noqa: F401
-# from .engines import EngineSequoiaIE  # noqa: F401
